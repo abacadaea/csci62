f4b40d96299616d6e0beacaeabb89dda48ea68da
Ray Li <rayyli02@gmail.com>
2023-12-11 20:05:47 -0800

    add bfs.cpp

---
+++ 1graphs/bfs.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+
+void print_queue(std::queue<int> q)
+{
+  while (!q.empty())
+  {
+    std::cout << q.front() << " ";
+    q.pop();
+  }
+  std::cout << std::endl;
+}
+
+class Graph {
+public:
+  // pre: none
+  // post: an empty Graph has been created
+  Graph() {}
+
+  // pre: none
+  // post: returns the number of vertices in this Graph
+  int n() const {
+    return adjLists_.size();
+  }
+
+  // pre: none
+  // post: returns the number of edges in this Graph
+  int m() const {
+    int count = 0;
+    for(auto list : adjLists_) {
+        count += list.size();
+    }
+    return count / 2;
+  }
+
+  // pre: none
+  // post: a new vertex with label n() has been added to this Graph
+  void addVertex() {
+    adjLists_.push_back(std::set<int>{});
+  }
+
+  // pre: i < n() && j < n()
+  // post: edge {i, j} has been added to this Graph
+  //       if it is not an edge already
+  void addEdge(int i, int j) {
+    adjLists_[i].insert(j);
+    adjLists_[j].insert(i);
+  }
+
+  // pre: i < n() && j < n()
+  // post: edge {i, j} is not in this Graph
+  void deleteEdge(int i, int j) {
+    adjLists_[i].erase(j); 
+    adjLists_[j].erase(i); 
+  }
+
+
+  // what does this print out?
+  void BFS(int source) {
+    std::queue<int> q;
+    std::vector<bool> visited (n(), 0);
+
+    visited[source] = true;
+    q.push(source);
+
+    while (q.size() > 0) {
+      int cur = q.front();
+      q.pop();
+      for (auto neighbor : adjLists_[cur]) {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          q.push(neighbor);
+        }
+      }
+      // print statements added
+      std::cout << "cur: " << cur << std::endl;
+      std::cout << "q: "; print_queue(q);
+      std::cout << "visited: ";
+      for (int i = 0; i < n(); i ++) {
+        std::cout << i << (visited[i] ? "T" : "F") << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+ private:
+  // assumption: vertices are numbered 0, 1, ..., n-1
+  std::vector<std::set<int> > adjLists_;
+};
+
+
+int main () {
+  Graph G;
+  G.addVertex();
+  G.addVertex();
+  G.addVertex();
+  G.addVertex();
+  G.addVertex();
+  G.addEdge(0,1);
+  G.addEdge(1,2);
+  G.addEdge(2,3);
+  G.addEdge(1,3);
+  G.addEdge(1,4);
+  G.addEdge(4,3);
+  G.addEdge(0,4);
+
+  G.BFS(2);
+}

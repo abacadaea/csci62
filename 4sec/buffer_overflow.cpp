194eebeee3758d9ac27af65b515d4ba11b42f556
Ray Li <rli6@scu.edu>
2024-02-23 14:41:00 -0800

    Change buffer overflow

--- 4sec/buffer_overflow.cpp
+++ 4sec/buffer_overflow.cpp
@@ -5,31 +5,24 @@ using namespace std;
 int main() {
   char str1[] = "HEY THERE";
   char str2[] = "ALOHA!";
-  char str3[] = "HOWDY";
-  char str4[] = "HELLO WORLD";
+  char str3[] = "HELLO WORLD";
 
   cout << "Before:\n";
   cout << "str1: " << str1 << endl;
   cout << "str2: " << str2 << endl;
   cout << "str3: " << str3 << endl;
-  cout << "str4: " << str4 << endl;
+
+  cout << "\nAddresses:\n";
+  cout << "str1: " << &str1 << endl;
+  cout << "str2: " << &str2 << endl;
+  cout << "str3: " << &str3 << endl;
 
   int i;
-  for (i = 0; str4[i]!='\0'; i++) {  // until the end of str3
-    str2[i] = str4[i];
+  for (i = 0; str3[i]!='\0'; i++) {  // until the end of str4
+    str2[i] = str3[i];
   }
   cout << "\nAfter:\n";
   cout << "str1: " << str1 << endl;
   cout << "str2: " << str2 << endl;
   cout << "str3: " << str3 << endl;
-  cout << "str4: " << str4 << endl;
-
-  for (i = 0; str4[i]!='\0'; i++) {  // until the end of str3
-    str3[i] = str4[i];
-  }
-  cout << "\nAfter:\n";
-  cout << "str1: " << str1 << endl;
-  cout << "str2: " << str2 << endl;
-  cout << "str3: " << str3 << endl;
-  cout << "str4: " << str4 << endl;
 }

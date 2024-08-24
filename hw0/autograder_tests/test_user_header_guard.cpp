98acee05ceec438dc064d4bf011b46ebf5e37462
Ray Li <rli6@scu.edu>
2024-08-24 11:14:04 -0700

    Add hw0 tests

---
+++ hw0/autograder_tests/test_user_header_guard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "user.h"
+#include "user.h" // check no compile error on double include
+
+using namespace std;
+
+int main () {
+  User* u = new User ();
+}
+

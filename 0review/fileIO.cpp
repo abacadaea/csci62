440b4935a7f6bc4acfbb29326ef59ed764efcb6d
Ray Li <rli6@scu.edu>
2024-09-19 11:42:28 -0700

    add fileIO and vectorset.cpp

---
+++ 0review/fileIO.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  ifstream fileIn("myfile.txt");
+  stringstream strIn("Line from stringstream: blahblah\nblahblah");
+/*
+  string myLine;
+  getline(cin, myLine);
+  cout << "getline(cin,myLine): " << myLine << endl;
+  getline(fileIn, myLine);
+  cout << "getline(fileIn,myLine): " << myLine << endl;
+  getline(strIn, myLine);
+  cout << "getline(strIn,myLine): " << myLine << endl;
+*/
+
+  string myWord;
+  cin >> myWord;
+  cout << "cin>>myWord: " << myWord << endl;
+  fileIn >> myWord;
+  cout << "fileIn>>myWord: " << myWord << endl;
+  strIn >> myWord;
+  cout << "strIn>>myWord: " << myWord << endl;
+}
---
+++ 0review/vectorset.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+
+int main () {
+  // vector demo
+  vector<int> arr;
+  arr.push_back(0);
+  arr.push_back(0);
+  arr.push_back(1);
+  arr[1] = 2;
+  for (int i = 0; i < arr.size(); i ++) {
+    cout << arr [i] << " ";
+  }
+  cout << endl;
+
+  // set demo
+  set<int> s;
+  s.insert(5);
+  s.insert(5);
+  s.insert(2);
+  s.insert(3);
+  cout << s.size() << endl;
+  for (auto it: s) {
+    cout << it << " ";
+  }
+  cout << endl;
+}

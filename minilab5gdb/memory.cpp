2e1231d8be583ca586a6a5962478fc9edf80318f
Ray Li <rli6@scu.edu>
2024-09-20 17:52:51 -0700

    Update gdb minilab

---
+++ minilab5gdb/memory.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void foo(int *n) {
+  *n = *n + 10;
+}
+
+
+int main() {
+  int x = 10;
+  int y = 20;
+  int z = 30;
+  int w = 256;
+
+  int* a  = new int(5);
+  int* b  = new int(10);
+  int* c  = new int(10);
+
+  foo(&x);
+  printf("x: %d\n", x);
+  foo(a);
+  printf("*a: %d\n", *a);
+}

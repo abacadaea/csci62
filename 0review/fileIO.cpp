e29bb728287f8e48207bdfdce319cc6069695c2b
Ray Li <rli6@scu.edu>
2024-09-24 15:23:10 -0700

    update fileIO

--- 0review/fileIO.cpp
+++ 0review/fileIO.cpp
@@ -6,23 +6,60 @@
 using namespace std;
 
 int main() {
+  string myLine, myWord;
+
+  // standard input
+  cin >> myWord; // standard input
+  cout << "cin>>myWord: " << myWord << endl;
+
+  // file input 
   ifstream fileIn("myfile.txt");
-  stringstream strIn("Line from stringstream: blahblah\nblahblah");
-/*
-  string myLine;
+  fileIn >> myWord; 
+  cout << "fileIn>>myWord: " << myWord << endl;
+
+  // string input
+  stringstream strIn("    apple \t orange\nbanana blueberry");
+  strIn >> myWord; // string input (stringstream)
+  cout << "strIn>>myWord: " << myWord << endl;
+  strIn >> myWord; // string input (stringstream)
+  cout << "strIn>>myWord: " << myWord << endl;
+  strIn >> myWord; // string input (stringstream)
+  cout << "strIn>>myWord: " << myWord << endl;
+
+  // getline
+  /*
   getline(cin, myLine);
   cout << "getline(cin,myLine): " << myLine << endl;
+
+  ifstream fileIn("myfile.txt");
   getline(fileIn, myLine);
   cout << "getline(fileIn,myLine): " << myLine << endl;
+  getline(fileIn, myLine);
+  cout << "getline(fileIn,myLine): " << myLine << endl;
+  getline(fileIn, myLine);
+  cout << "getline(fileIn,myLine): " << myLine << endl;
+  getline(fileIn, myLine);
+  cout << "getline(fileIn,myLine): " << myLine << endl;
+
+  stringstream strIn("apple orange\nbanana blueberry");
   getline(strIn, myLine);
   cout << "getline(strIn,myLine): " << myLine << endl;
-*/
+  */
 
-  string myWord;
-  cin >> myWord;
-  cout << "cin>>myWord: " << myWord << endl;
-  fileIn >> myWord;
-  cout << "fileIn>>myWord: " << myWord << endl;
-  strIn >> myWord;
-  cout << "strIn>>myWord: " << myWord << endl;
+  /*
+  ifstream fileIn("myfile2.txt");
+  int x, sum = 0;
+  while(fileIn  >> x) {
+    sum += x;
+    cout << "x=" << x << ", sum=" << sum << endl;
+  }
+  cout << "sum=" << sum << endl;
+
+  stringstream strIn("1 2\n 1 2");
+  sum = 0;
+  while(strIn  >> x) {
+    sum += x;
+  }
+  cout << "sum=" << sum << endl; 
+  */
 }

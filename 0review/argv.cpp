4d1d6e472cf9ec4e8331a535f387155d0714f07d
Ray Li <rli6@scu.edu>
2024-09-25 10:32:46 -0700

    Update argv.cpp

--- 0review/argv.cpp
+++ 0review/argv.cpp
@@ -2,10 +2,21 @@
 
 using namespace std;
 
+/* 
+ // the usual way
+int main (int argc, char* argv[]) {
+  // some code
+}
+*/
+
 
 int main(int argc, char *argv[]) {
   cout << "Number of arguments: " << argc << endl;
   for (int i = 0; i < argc; i ++) {
     cout << "Argument " << i << ": " << argv[i] << endl;
   }
+
+  string s; // (C++ string)
+  char s2[4]; // char array (C-string)
+  char *s3 = s2; // C-strings are actually char-pointers (that point to the string)
 }

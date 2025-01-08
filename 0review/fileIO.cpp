da3e06163d2728b2e8343b661f4a3e5aa88ef298
Ray Li <rli6@scu.edu>
2025-01-08 10:17:54 -0800

    Update fileIO

--- 0review/fileIO.cpp
+++ 0review/fileIO.cpp
@@ -9,57 +9,55 @@ int main() {
   string myLine, myWord;
 
   // standard input
-  cin >> myWord; // standard input
-  cout << "cin>>myWord: " << myWord << endl;
+  //cin >> myWord; // standard input
+  //cout << "cin>>myWord: " << myWord << endl;
+  //cin >> myWord; // standard input
+  //cout << "cin>>myWord: " << myWord << endl;
 
   // file input 
-  ifstream fileIn("myfile.txt");
-  fileIn >> myWord; 
-  cout << "fileIn>>myWord: " << myWord << endl;
+  //ifstream fileIn("myfile.txt");
+  //fileIn >> myWord; 
+  //cout << "fileIn>>myWord: " << myWord << endl;
+  //fileIn >> myWord; 
+  //cout << "fileIn>>myWord: " << myWord << endl;
+  //fileIn >> myWord; 
+  //cout << "fileIn>>myWord: " << myWord << endl;
+  //fileIn >> myWord; 
+  //cout << "fileIn>>myWord: " << myWord << endl;
+  
+  // getline
+  //getline(cin, myLine);
+  //cout << "getline(cin,myLine): " << myLine << endl;
+  //getline(cin, myLine);
+  //cout << "getline(cin,myLine): " << myLine << endl;
+  //getline(cin, myLine);
+  //cout << "getline(cin,myLine): " << myLine << endl;
+
+  //ifstream fileIn("myfile.txt");
+  //getline(fileIn, myLine);
+  //cout << "getline(fileIn,myLine): " << myLine << endl;
+  //getline(fileIn, myLine);
+  //cout << "getline(fileIn,myLine): " << myLine << endl;
+  //getline(fileIn, myLine);
+  //cout << "getline(fileIn,myLine): " << myLine << endl;
+  //getline(fileIn, myLine);
+  //cout << "getline(fileIn,myLine): " << myLine << endl;
+  //getline(fileIn, myLine);
+  //cout << "getline(fileIn,myLine): " << myLine << endl;
+
+  //ifstream fileIn("myfile2.txt");
+  //int x, sum = 0;
+  //while(fileIn  >> x) { // read until no numbers left to read
+  //  sum += x;
+  //  cout << "x=" << x << ", sum=" << sum << endl;
+  //}
+  //cout << "sum=" << sum << endl;
 
   // string input
-  stringstream strIn("    apple \t orange\nbanana blueberry");
-  strIn >> myWord; // string input (stringstream)
-  cout << "strIn>>myWord: " << myWord << endl;
-  strIn >> myWord; // string input (stringstream)
-  cout << "strIn>>myWord: " << myWord << endl;
-  strIn >> myWord; // string input (stringstream)
-  cout << "strIn>>myWord: " << myWord << endl;
-
-  // getline
-  /*
-  getline(cin, myLine);
-  cout << "getline(cin,myLine): " << myLine << endl;
-
-  ifstream fileIn("myfile.txt");
-  getline(fileIn, myLine);
-  cout << "getline(fileIn,myLine): " << myLine << endl;
-  getline(fileIn, myLine);
-  cout << "getline(fileIn,myLine): " << myLine << endl;
-  getline(fileIn, myLine);
-  cout << "getline(fileIn,myLine): " << myLine << endl;
-  getline(fileIn, myLine);
-  cout << "getline(fileIn,myLine): " << myLine << endl;
-
-  stringstream strIn("apple orange\nbanana blueberry");
-  getline(strIn, myLine);
-  cout << "getline(strIn,myLine): " << myLine << endl;
-  */
-
-  /*
-  ifstream fileIn("myfile2.txt");
   int x, sum = 0;
-  while(fileIn  >> x) {
-    sum += x;
-    cout << "x=" << x << ", sum=" << sum << endl;
-  }
-  cout << "sum=" << sum << endl;
-
-  stringstream strIn("1 2\n 1 2");
-  sum = 0;
+  stringstream strIn("1 2 1 2");
   while(strIn  >> x) {
     sum += x;
   }
   cout << "sum=" << sum << endl; 
-  */
 }
---
+++ 0review/ref.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int a = 1;
+  int x = a;
+  x = 2;
+  cout << "a=" << a << endl; 
+  cout << "x=" << x << endl; 
+}

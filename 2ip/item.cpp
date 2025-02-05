ed820acf0e2ff29fcfe9b06337a27e63b358f986
Ray Li <rli6@scu.edu>
2025-02-05 10:33:52 -0800

    Update item.cpp

--- 2ip/item.cpp
+++ 2ip/item.cpp
@@ -30,37 +30,20 @@ bool itemCmp(Item* i1, Item* i2) {
 int main()
 {
   Item i = Item(10.00);//One item at $10.00.
-  DiscountItem di = DiscountItem(11.00, 10);//One item at $11.00 with a 10% discount=$9.90
   cout << i.bill() << endl;
+
+  DiscountItem di = DiscountItem(11.00, 10);//One item at $11.00 with a 10% discount=$9.90
   cout << di.bill() << endl;
-                                                    
-  //What do you think will happen?
+  
+  //What  happens?
   Item i2 = DiscountItem(11.00, 10);
   cout<<"i2.bill()=" <<i2.bill()<<endl;//bill is the function we want to be able to do polymorphism with
 
-  // What is the difference
-  Item* ip = & di; // sp points to "stack" memory
-  cout << "ip->bill()=" << ip->bill()<<endl;
-
-
-  Item* ip2 = new DiscountItem(11.00, 10); // sp2 points to "heap" memory
+  //What happens?
+  Item* ip2 = new DiscountItem(11.00, 10); 
   cout << "ip2->bill()=" << ip2->bill()<<endl;
 
-  vector<Item> v;
-  v.push_back(i);
-  v.push_back(di);
-  for (auto i : v) {
-    cout << i.bill() << endl; // only calls item bill function
-  }
-
   vector<Item*> vp;
-  vp.push_back(&i);
-  vp.push_back(&di);
-  for (auto i : vp){ 
-    cout << i->bill() << endl;
-  }
-
-  vp.clear();
   vp.push_back(new Item(10));
   vp.push_back(new Item(9));
   vp.push_back(new Item(90));
@@ -71,15 +54,15 @@ int main()
   vp.push_back(new DiscountItem(11,9));
   vp.push_back(new DiscountItem(10,9));
   cout << "\nBefore sort\n";
-  for (auto i : vp) {
+  for (Item* i : vp) {
     i->print();
-    //cout << i->bill() << endl;
   } 
-  cout << "\nAfter sort\n";
+
   sort(vp.begin(), vp.end(), itemCmp);
-  for (auto i : vp) {
+
+  cout << "\nAfter sort\n";
+  for (Item* i : vp) {
     i->print();
-    //cout << i->bill() << endl;
-  } 
+  }
 }
 

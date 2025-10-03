1d8850c94e08621a119282647076b0f237e83389
Ray Li <rli6@scu.edu>
2025-10-03 14:49:33 -0700

    Update bfs3.cpp

--- 1graph/bfs3.cpp
+++ 1graph/bfs3.cpp
@@ -5,6 +5,17 @@
 
 using namespace std;
 
+void print_queue(queue<int> q)
+{
+  while (!q.empty())
+  {
+    cout << q.front() << " ";
+    q.pop();
+  }
+  cout << endl;
+}
+
+
 class Graph {
 public:
   // pre: none
@@ -57,6 +68,15 @@ public:
     visited[from] = true;
     q.push(from);
 
+    cout << "=======Before loop======\n";
+    cout << "q: " ;
+    print_queue(q);
+    cout << "prev: ";
+    for (int i = 0; i < n(); i ++) {
+      cout << prev[i] << " ";
+    }
+    cout << endl;
+
     while (q.size() > 0) {
       int cur = q.front();
       q.pop();
@@ -67,7 +87,10 @@ public:
           q.push(neighbor);
         }
       }
+      cout << "=============\n";
       cout << "cur: " << cur << endl;
+      cout << "q: " ;
+      print_queue(q);
       cout << "prev: ";
       for (int i = 0; i < n(); i ++) {
         cout << prev[i] << " ";
@@ -76,10 +99,17 @@ public:
     }
     vector<int> output;
     int cur = to;
-    while (cur != -1) {
+    // version 1
+    while (cur != -1 ) {
       output.push_back(cur);
       cur = prev[cur];
     }
+    // version 2
+    //while (cur != from ) {
+    //  output.push_back(cur);
+    //  cur = prev[cur];
+    //}
+    //output.push_back(from);
     reverse(output.begin(), output.end());
     return output;
   }
@@ -129,7 +159,7 @@ int main () {
     G.addEdge(4,3);
     G.addEdge(0,4);
 
-    std::vector<int> path = G.shortestPath(5,2);
+    std::vector<int> path = G.shortestPath(4,2);
     std::cout << "Number of Vertices on Path: " << path.size() << std::endl << "Path: ";
     for (auto v : path) {
       std::cout << v << " ";
